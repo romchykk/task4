eacddf7e910a95677aca92a68c6de3b587216f1c
Tvoe Imya <rmihac53@gmail.com>
2025-11-09 17:22:52 +0200

    My first commit with C++ project and Jenkinsfile

---
+++ vs_mkr_test1/sort.h
@@ -0,0 +1,39 @@
+#ifndef SORT
+#define SORT
+#include "stringProcessor.h"
+#include <string.h> // Потрібно для strcmp
+
+void swap(char** a, char** b)
+{
+    char* temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void quick_sort(char** arr, size_t length)
+{
+    if (length <= 1)
+    {
+        return;
+    }
+
+    size_t piv = 0;
+    size_t last = length - 1;
+
+    // Використовуємо стандартну strcmp, як у твого друга
+    for (size_t i = 0; i < length; i++)
+    {
+        if (strcmp(arr[i], arr[last]) < 0)
+        {
+            swap(arr + i, arr + piv);
+            piv++;
+        }
+    }
+
+    swap(arr + piv, arr + last);
+
+    quick_sort(arr, piv);
+    quick_sort(arr + piv + 1, length - piv - 1);
+}
+
+#endif // !SORT
